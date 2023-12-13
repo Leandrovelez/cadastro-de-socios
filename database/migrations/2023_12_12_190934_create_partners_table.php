53d9aacebff3dfa23c2266f0f6f06c528388ef80
Leandro Velez <leandrovelez55@gmail.com>
2023-12-13 14:24:35 -0300

    CRUD and login working

---
+++ database/migrations/2023_12_12_190934_create_partners_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('partners', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 250);
+            $table->string('email', 250);
+            $table->enum('type', ['Silver', 'Gold']);
+            $table->string('cpf', 14);
+            $table->string('cep', 10);
+            $table->string('state', 100);
+            $table->string('city', 100);
+            $table->string('address', 100);
+            $table->integer('number');
+            $table->string('complement', 100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('partners');
+    }
+};
